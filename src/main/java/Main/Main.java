1547aa8ba891e3511467e5b920457b716d625620
shramko <serhiyovich@gmail.com>
2018-06-25 14:52:31 +0300

    Create Main class

---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,42 @@
+package Main;
+
+import animals.inheritors.Kitty;
+import enumerations.CatPosition;
+import enumerations.CuteLevel;
+import enumerations.DayPart;
+import food.EatCharacteristic;
+import food.KittyCat;
+import humans.Owner;
+import org.apache.log4j.BasicConfigurator;
+
+public class Main {
+    public static void main(String[] args) {
+        BasicConfigurator.configure();
+        Kitty kitty  = new Kitty("Puh",5);
+        kitty.getCuteLevel();
+        kitty.setOwner(new Owner(){//todo
+            {
+                setFirstName("Vitalii");
+                setSecondName("Shramko");
+                setAge(25);
+            }
+        });
+        kitty.setCuteLevel(CuteLevel.GOOD_CAT);
+        kitty.setKittyCat(new KittyCat("meat",600));
+
+        /*Owner owner = new Owner();
+        owner.setFirstName("Vitalii");
+        owner.setSecondName("Shramko");
+
+        kitty.getOwner();*/
+
+        KittyCat kittyCat = new KittyCat("meat",600);
+        EatCharacteristic eatCharacteristic = EatCharacteristic.builder()
+                .amountFoodEaten(200)
+                .catPosition(CatPosition.SIT)
+                .dayPart(DayPart.DAY)
+                .createCharacteristic();
+        kitty.eatFood(eatCharacteristic);
+        kitty.killCat();
+    }
+}
